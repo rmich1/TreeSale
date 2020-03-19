21c21c257cf286b3a2e5b0163379232c1f72a18f
Becky <anwebsa91@gmail.com>
2020-03-18 20:26:38 -0400

    Tree partially finished

--- model/Tree.java
+++ model/Tree.java
@@ -8,29 +8,30 @@ import java.util.Enumeration;
 import java.util.Properties;
 import java.util.Vector;
 
-public class Scout extends EntityBase {
+public class Tree extends EntityBase {
     //Table Name Scout
-    private static final String myTableName = "Scout";
+    private static final String myTableName = "Tree";
 
     // GUI Components
 
     private String updateStatusMessage = "";
 
     protected Properties dependencies;
+
     //Empty Constructor
-    public Scout(){
+    public Tree() {
         super(myTableName);
         setDependencies();
         persistentState = new Properties();
     }
-    //Looks for a scoutID this will never be used
-    public Scout(String scoutId)
+
+    public Tree(String barcode)
             throws InvalidPrimaryKeyException {
         //calling the constructor to the EntityBase by passing in the name of the table
         super(myTableName);
         setDependencies();
         //SQL Query to get all scouts with scoutID's
-        String query = "SELECT * FROM " + myTableName + " WHERE (scoutId = " + scoutId + ")";
+        String query = "SELECT * FROM " + myTableName + " WHERE (barcode = " + barcode + ")";
 
         //creates a vector of all objects where the information from the table is related to the primary key
         Vector<Properties> allDataRetrieved = getSelectQueryResult(query);
@@ -41,8 +42,8 @@ public class Scout extends EntityBase {
 
             // There should be EXACTLY one scout
             if (size != 1) {
-                throw new InvalidPrimaryKeyException("Multiple scouts matching id : "
-                        + scoutId + " found.");
+                throw new InvalidPrimaryKeyException("Multiple trees matching barcode : "
+                        + barcode + " found.");
             } else {
                 // copy all the retrieved data into persistent state
                 Properties retrievedAccountData = allDataRetrieved.elementAt(0);
@@ -65,13 +66,14 @@ public class Scout extends EntityBase {
         }
         // If no account found for this user name, throw an exception
         else {
-            throw new InvalidPrimaryKeyException("No scouts matching id : "
-                    + scoutId + " found.");
+            throw new InvalidPrimaryKeyException("No trees matching barcode : "
+                    + barcode + " found.");
         }
 
     }
+
     //This is to add something into the database that doesn't exist
-    public Scout(Properties props) {
+    public Tree(Properties props) {
         super(myTableName);
 
         setDependencies();
@@ -94,25 +96,11 @@ public class Scout extends EntityBase {
     private void setDependencies() {
 
         dependencies = new Properties();
-        //dependencies.setProperty("InsertNewScout", "InsertError");
-        //dependencies.setProperty("SearchScouts", "SearchError");
-
         myRegistry.setDependencies(dependencies);
     }
+
     //-----------------------------------------------------------------------------------
     public Object getState(String key) {
-
-       /* if (key.equals("scoutId") == true)
-            return persistentState.getProperty("scoutId");
-        if (key.equals("UpdateStatusMessage") == true)
-            return updateStatusMessage;
-        if(key.equals("InsertError") == true)
-            return updateStatusMessage;
-        if(key.equals("SearchError") == true)
-            return updateStatusMessage;
-
-
-    */
 
         return persistentState.getProperty(key);
     }
@@ -129,11 +117,6 @@ public class Scout extends EntityBase {
             mySchema = getSchemaInfo(tableName);
         }
     }
-   /* public void updateState(String key, Object value)
-    {
-        stateChangeRequest(key, value);
-    }
-    */
 
     //-----------------------------------------------------------------------------------
     //Save the Scout into the database
@@ -145,64 +128,46 @@ public class Scout extends EntityBase {
     //-----------------------------------------------------------------------------------
     private void updateStateInDatabase() {
         try {
-            if (persistentState.getProperty("scoutId") != null) {
+            if (persistentState.getProperty("barcode") != null) {
                 Properties whereClause = new Properties();
-                whereClause.setProperty("scoutId",
-                        persistentState.getProperty("scoutId"));
+                whereClause.setProperty("barcode",
+                        persistentState.getProperty("barcode"));
                 updatePersistentState(mySchema, persistentState, whereClause);
-                updateStatusMessage = "Scout information for scoutId: " + persistentState.getProperty("scoutId") + " updated successfully in database!";
+                updateStatusMessage = "Tree information for barcode: " + persistentState.getProperty("barcode") + " updated successfully in database!";
             } else {
-                Integer scoutId =
+                Integer barcode =
                         insertAutoIncrementalPersistentState(mySchema, persistentState);
-                persistentState.setProperty("scoutId", "" + scoutId.intValue());
-                updateStatusMessage = "Scout data for new Scout: " + persistentState.getProperty("scoutId")
+                persistentState.setProperty("barcode", "" + barcode.intValue());
+                updateStatusMessage = "Tree data for new Tree: " + persistentState.getProperty("barcode")
                         + " added successfully to database!";
             }
         } catch (SQLException ex) {
-            updateStatusMessage = "Error in adding scout data to database!";
+            updateStatusMessage = "Error in adding tree data to database!";
         }
         System.out.println(updateStatusMessage + "\n");
     }
+
     //-----------------------------------------------------------------------------------
-    //Compare two scoutss in the database by scoutID
-    public static int compare(Scout a, Scout b) {
-        String aNum = (String) a.getState("scoutId");
-        String bNum = (String) b.getState("scoutId");
+    //Compare two scouts in the database by scoutID
+    public static int compare(Tree a, Tree b) {
+        String aNum = (String) a.getState("barcode");
+        String bNum = (String) b.getState("barcode");
 
         return aNum.compareTo(bNum);
     }
+
     //-----------------------------------------------------------------------------------
     public Vector<String> getEntryListView() {
         Vector<String> v = new Vector<String>();
 
-        v.addElement(persistentState.getProperty("scoutId"));
-        v.addElement(persistentState.getProperty("firstName"));
-        v.addElement(persistentState.getProperty("middleName"));
-        v.addElement(persistentState.getProperty("lastName"));
-        v.addElement(persistentState.getProperty("dateOfBirth"));
-        v.addElement(persistentState.getProperty("phoneNumber"));
-        v.addElement(persistentState.getProperty("email"));
-        v.addElement(persistentState.getProperty("status"));
-        v.addElement(persistentState.getProperty("dateStatusUpdated"));
-        v.addElement(persistentState.getProperty("troopId"));
-
-
 
         return v;
     }
+
     //-----------------------------------------------------------------------------------
-    public String toString(){
-        return("Scout Id: " +   persistentState.getProperty("scoutId")+
-               "First Name: " + persistentState.getProperty("firstName") +
-                "Middle Name: " + persistentState.getProperty("middleName") +
-                "Last Name: " +  persistentState.getProperty("lastName") +
-                "Date Of Birth: " + persistentState.getProperty("dateOfBirth") +
-                "Phone Number: " + persistentState.getProperty("phoneNumber") +
-                "Email: " + persistentState.getProperty("email") +
-                "Status: " + persistentState.getProperty("status") +
-                "Date Status Updated: " + persistentState.getProperty("dateStatusUpdated") +
-                "TroopID: " + persistentState.getProperty("troopId"));
+    public String toString() {
+        return "";
+
 
     }
-
 }
