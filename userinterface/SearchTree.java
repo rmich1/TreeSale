68d4f7938c749fb30fc909e39739737b015cd7b2
Becky Michael <rmich1@brockport.edu>
2020-03-21 10:45:19 -0400

    Before Updating update/delete button

---
+++ userinterface/SearchTree.java
@@ -0,0 +1,199 @@
+// specify the package
+package userinterface;
+
+// system imports
+
+import javafx.event.Event;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Popup;
+import javafx.stage.Stage;
+
+import java.util.Date;
+import java.util.Properties;
+import java.util.Vector;
+
+// project imports
+import impresario.IModel;
+import model.Scout;
+import model.ScoutCollection;
+import model.TreeCollection;
+
+/** The view that has the first name, last name and email the user uses to search for a scout collection */
+//==============================================================
+public class SearchTree extends View {
+
+    // Model
+
+    // GUI components
+    //Text Fields
+    private Properties prop;
+    private TextField barcodeTF;
+
+    //Buttons
+    private Button cancelButton;
+    private Button searchButton;
+    private TreeCollection treeList = new TreeCollection();
+
+
+
+    // For showing error message
+    private userinterface.MessageView statusLog;
+
+
+    // constructor for this class -- takes a model object
+    //----------------------------------------------------------
+    public SearchTree(IModel updateTree) {
+        super(updateTree, "SearchTree");
+
+        // create a container for showing the contents
+        VBox container = new VBox(10);
+        container.setPadding(new Insets(15, 5, 5, 5));
+
+        // create our GUI components, add them to this panel
+        container.getChildren().add(createTitle());
+        container.getChildren().add(createFormContent());
+
+        // Error message area
+        container.getChildren().add(createStatusLog("                          "));
+
+        getChildren().add(container);
+
+        populateFields();
+    }
+
+
+    // Create the label (Text) for the title
+    //-------------------------------------------------------------
+    private Node createTitle() {
+
+        Text titleText = new Text("Update Tree");
+        titleText.setWrappingWidth(300);
+        titleText.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+        titleText.setTextAlignment(TextAlignment.CENTER);
+        titleText.setFill(Color.BLACK);
+
+        return titleText;
+    }
+
+    // Create the main form content
+    //-------------------------------------------------------------
+    private VBox createFormContent() {
+        VBox vbox = new VBox(30);
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+
+        //TextFields
+        barcodeTF = new TextField();
+        //Labels
+        Label barcodeLabel = new Label("Barcode: ");
+        Label prompt = new Label("Enter Barcode");
+        searchButton = new Button("Search");
+
+        grid.add(prompt, 0, 0);
+        grid.add(barcodeLabel, 0, 1);
+        grid.add(barcodeTF, 1, 1);
+
+
+
+        searchButton.setOnAction(e -> processAction(e));
+
+
+        cancelButton = new Button("Back");
+        cancelButton.setOnAction(e -> myModel.stateChangeRequest("Return", null));
+
+        HBox btnContainer = new HBox(100);
+        btnContainer.setAlignment(Pos.CENTER);
+        btnContainer.getChildren().add(cancelButton);
+        btnContainer.getChildren().add(searchButton);
+
+
+        vbox.getChildren().add(grid);
+        vbox.getChildren().add(btnContainer);
+
+        return vbox;
+    }
+
+    // Create the status log field
+    //-------------------------------------------------------------
+    private userinterface.MessageView createStatusLog(String initialMessage) {
+        statusLog = new userinterface.MessageView(initialMessage);
+
+        return statusLog;
+    }
+
+    //-------------------------------------------------------------
+    public void populateFields() {
+
+
+    }
+
+    // process events generated from our GUI components
+    //-------------------------------------------------------------
+    public void processAction(Event evt) {
+        //myModel.statechangeRequest goes to the ScoutEditTransaction Class ScoutCollectionTransaction
+
+        clearErrorMessage();
+
+        if(barcodeTF.getText().length() == 0){
+            displayErrorMessage("Enter Barcode");
+        }
+        else{
+                myModel.stateChangeRequest("UpdateTree", barcodeTF.getText());
+        }
+    }
+
+
+
+
+
+
+    /**
+     * Required by interface, but has no role here
+     */
+    //---------------------------------------------------------
+    public void updateState(String key, Object value) {
+
+    }
+
+    /**
+     * Display error message
+     */
+    //----------------------------------------------------------
+    public void displayErrorMessage(String message) {
+        statusLog.displayErrorMessage(message);
+    }
+
+    public void displayMessage(String message) {
+        statusLog.displayMessage(message);
+    }
+
+
+
+    /**
+     * Clear error message
+     */
+    //----------------------------------------------------------
+    public void clearErrorMessage() {
+        statusLog.clearErrorMessage();
+    }
+}
+
